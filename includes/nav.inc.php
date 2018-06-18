ebe33d48c1219d8f58c4356c01a77aaa33844b4d
30218361 <30218361@bsdc.ac.uk>
2018-06-18 16:03:51 +0100

    Form changes

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,5 +1,9 @@
 <div class="nav">
 	<div class="holder">
 		<div class="title"><a href="index.php">PC Clinic</a></div>
+		<div class="links">
+			<a href="workedOn.php">Lukes meme</a>
+			<a href="search.php">Search</a>
+		</div>
 	</div>
 </div>
--- workedOn.php
+++ workedOn.php
@@ -1,70 +1,92 @@
 <?php
-    
+
+    require_once("includes/header.inc.php");
+	
 ?>
 <html>
     <body>
-		<div class="nav">
-			<div class="holder">
-                <div class="title"><h1>PC_Clinic</h1></div>
-			</div>
-		</div>
+		<?php require_once("includes/nav.inc.php"); ?>
 		<form class="form" method="POST">
+			<div class="title">Updates</div>
 			<input type="text" placeholder="Worked on by" name="workedOn">
-            
-            <br><br>
-            
 			<input type="date" placeholder="Date of work carried out" name="dateCarriedOut">
-            
-            <br><br>
-            
-            <h3>Hardware Check</h3>
-            <input type="checkbox" name="HHD">
-            <label for="HHD">HHD</label>
-            
-            <input type="checkbox" name="Monitor">
-            <label for="Monitor">Monitor</label>
-            
-            <input type="checkbox" name="RAM">
-            <label for="RAM">RAM</label>
-            
-            <input type="checkbox" name="Motherboard">
-            <label for="Motherboard">Motherboard</label>
-            
-            <h3>Software Check</h3>
-            <input type="checkbox" name="upToDate">
-            <label for="upToDate">Up To Date</label>
-            
-            <input type="checkbox" name="virusScanner">
-            <label for="virusScanner">Virus Scanner</label>
-            
-            <h3>Network Check</h3>
-            <input type="checkbox" name="wired">
-            <label for="wired">Wired</label>
-            
-            <input type="checkbox" name="wireless">
-            <label for="wireless">Wireless</label>
-            
-            <input type="checkbox" name="connection">
-            <label for="connection">Connection</label>
-            
-            <input type="checkbox" name="internet">
-            <label for="internet">Internet</label>
-            
-            <input type="checkbox" name="emailConfig">
-            <label for="emailConfig">E-mail Configuration</label>
-            
-            <h3>Additional Information</h3>
+            <div class="subtitle">Hardware Check</div>
+			
+			<div class="row">
+				<input type="checkbox" name="HDD">
+				<label for="HDD">HDD</label>
+			</div>
+			
+			<div class="row">
+				<input type="checkbox" name="Monitor">
+				<label for="Monitor">Monitor</label>
+            </div>
+			
+			<div class="row">
+				<input type="checkbox" name="RAM">
+				<label for="RAM">RAM</label>
+            </div>
+			
+			<div class="row">
+				<input type="checkbox" name="Motherboard">
+				<label for="Motherboard">Motherboard</label>
+            </div>
+			
+            <div class="subtitle">Software Check</div>
+			
+			<div class="row">
+				<input type="checkbox" name="upToDate">
+				<label for="upToDate">Up To Date</label>
+            </div>
+			
+			<div class="row">
+				<input type="checkbox" name="virusScanner">
+				<label for="virusScanner">Virus Scanner</label>
+            </div>
+			
+            <div class="subtitle">Network Check</div>
+			
+			<div class="row">
+				<input type="checkbox" name="wired">
+				<label for="wired">Wired</label>
+            </div>
+			
+			<div class="row">
+				<input type="checkbox" name="wireless">
+				<label for="wireless">Wireless</label>
+            </div>
+			
+			<div class="row">
+				<input type="checkbox" name="connection">
+				<label for="connection">Connection</label>
+            </div>
+			
+			<div class="row">
+				<input type="checkbox" name="internet">
+				<label for="internet">Internet</label>
+            </div>
+			
+			<div class="row">
+				<input type="checkbox" name="emailConfig">
+				<label for="emailConfig">E-mail Configuration</label>
+            </div>
+			
+            <div class="subtitle">Additional Information</div>
             <textarea rows="4" cols="50" placeholder="Additional Information"></textarea>
             
-            <h4>Status</h4>
-            <input type="radio" name="onGoing">
-            <lable for="ongGoing">On Going</lable>
-            
-            <input type="radio" name="complete">
-            <label for="complete">Complete</label>
-            
-            <input type="radio" name="cantRepair">
-            <label for="cantRepair">Can't Repair</label>
+            <div class="subtitle">Status</div>
+			<div class="row">
+				<input type="radio" name="status">
+				<label for="ongGoing">On Going</lable>
+            </div>
+			<div class="row">
+				<input type="radio" name="status">
+				<label for="complete">Complete</label>
+            </div>
+			<div class="row">
+				<input type="radio" name="status">
+				<label for="cantRepair">Can't Repair</label>
+			</div>
             
 			<br><br>
             <input type="submit" value="Submit" name="submit">
