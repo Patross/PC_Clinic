ed620d8dd324d5a11614a3965e8c1a5069826ac7
30219833 <30219833@bsdc.ac.uk>
2018-06-18 11:56:58 +0100

    Added index.php with form

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <main class="cont">
+            <form>
+                <input type=text placeholder="First Name" name="firstName">
+                <input type=text placeholder="Last Name" name="lastName">
+                <input type=email placeholder="Email Address" name="email">
+                <input type=tel placeholder="Phone Number" name="phone">
+                <input type=number placeholder="Item Serial Number" name="serial">
+                <input type=text placeholder="Address" name="address">
+                <input type=text placeholder="Postcode" name="postcode">
+                
+                <input type="submit" value="Submit" name="submit">
+            </form>
+        </main>
+    </body>
+</html>
