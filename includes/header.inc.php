fcb6728e0c1f49fae416d298d9fcd4a2912d64e0
30218361 <30218361@bsdc.ac.uk>
2018-06-18 12:14:25 +0100

    Form CSS

---
+++ includes/header.inc.php
@@ -0,0 +1,4 @@
+<head>
+	<title>PC_Clinic</title>
+	<link rel="stylesheet" href="css/styles.css" type="text/css"/>
+</head>
--- index.php
+++ index.php
@@ -1,17 +1,25 @@
+<?php
+
+require_once("includes/header.inc.php");
+
+?>
 <html>
     <body>
-        <main class="cont">
-            <form>
-                <input type=text placeholder="First Name" name="firstName">
-                <input type=text placeholder="Last Name" name="lastName">
-                <input type=email placeholder="Email Address" name="email">
-                <input type=tel placeholder="Phone Number" name="phone">
-                <input type=number placeholder="Item Serial Number" name="serial">
-                <input type=text placeholder="Address" name="address">
-                <input type=text placeholder="Postcode" name="postcode">
-                
-                <input type="submit" value="Submit" name="submit">
-            </form>
-        </main>
+		<div class="nav">
+			<div class="holder">
+				<div class="title">PC_Clinic</div>
+			</div>
+		</div>
+		<form class="form" method="POST">
+			<input type=text placeholder="First Name" name="firstName">
+			<input type=text placeholder="Last Name" name="lastName">
+			<input type=email placeholder="Email Address" name="email">
+			<input type=tel placeholder="Phone Number" name="phone">
+			<input type=number placeholder="Item Serial Number" name="serial">
+			<input type=text placeholder="Address" name="address">
+			<input type=text placeholder="Postcode" name="postcode">
+			
+			<input type="submit" value="Submit" name="submit">
+		</form>
     </body>
 </html>
