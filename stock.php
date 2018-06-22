e43936d2d454208b65f6254bc061581fe2be7274
Adam Curzon <curzzon@gmail.com>
2018-06-22 14:05:35 +0100

    stock

---
+++ stock.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once("includes/header.inc.php");
+require_once("includes/dbh.inc.php");
+
+$query = $conn->query("SELECT * FROM stock");
+$stock = $query->fetchAll();
+
+?>
+<html>
+    <body>
+		<?php require_once("includes/nav.inc.php"); ?>
+		<div class="holder">
+			<form class="form" action="includes/stock.inc.php" method="POST">
+				<div class="title">Add Stock</div>
+				<div class="subtitle">Item name</div>
+				<input type="text" placeholder="Item name" name="itemName">
+				<div class="subtitle">Item type</div>
+				<input type="text" placeholder="Item name" name="itemType">
+				<div class="subtitle">Item specs</div>
+				<input type="text" placeholder="Item name" name="itemSpecs">
+				<div class="subtitle">Item quantity</div>
+				<input type="text" placeholder="Item name" name="itemQuantity">
+				<input type="submit" value="Next"                    name="submit">
+			</form>
+			<table>
+				<tr>
+					<th>Item Name</th>
+					<th>Type</th>
+					<th>Specs</th>
+					<th>Quantity</th>
+				</tr>
+				<?php
+					for($i=0;$i<sizeOf($stock);$i++):
+				?>
+				<tr>
+					<td><?php echo $stock[$i]['item_name']?></td>
+					<td><?php echo $stock[$i]['item_type']?></td>
+					<td><?php echo $stock[$i]['item_specs']?></td>
+					<td><?php echo $stock[$i]['item_quantity']?></td>
+				</tr>
+				<?php
+					ENDFOR;
+				?>
+			</table>
+		</div>
+    </body>
+</html>
